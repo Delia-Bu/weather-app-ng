01b0631ded0be792eb416cfd4b915d296f1331a2
Delia-Bu <deliabulbucan@gmail.com>
2023-04-12 11:23:22 +0200

    cleaner code and deleted unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { City } from './models/interfaces/city.interface';
 import { GeoLocationService } from './services/geo-location.service';
 import { DatePipe } from '@angular/common';
 import { WeekDay } from './models/interfaces/weekDay.interface';
+import { weatherInterpretationCodes } from './models/constants/weather-codes.const';
+import { weatherIcons } from './models/constants/weather-icons.const';
 
 @Component({
   selector: 'app-root',
@@ -17,64 +19,8 @@ export class AppComponent {
   public currentCityWeather: Forecast | null = null;
   public showCitites = false;
   public currentCity: City | null = null;
-  public weatherInterpretationCodes: { [key: number]: string } = {
-    0: 'Clear sky',
-    1: 'Mainly clear, partly cloudy, and overcast',
-    2: 'Mainly clear, partly cloudy, and overcast',
-    3: 'Mainly clear, partly cloudy, and overcast',
-    45: 'Fog and depositing rime fog',
-    48: 'Fog and depositing rime fog',
-    51: 'Drizzle: Light, moderate, and dense intensity',
-    53: 'Drizzle: Light, moderate, and dense intensity',
-    55: 'Drizzle: Light, moderate, and dense intensity',
-    56: 'Freezing Drizzle: Light and dense intensity',
-    57: 'Freezing Drizzle: Light and dense intensity',
-    61: 'Rain: Slight, moderate and heavy intensity',
-    63: 'Rain: Slight, moderate and heavy intensity',
-    66: 'Freezing Rain: Light and heavy intensity',
-    67: 'Freezing Rain: Light and heavy intensity',
-    71: 'Snow fall: Slight, moderate, and heavy intensity',
-    73: 'Snow fall: Slight, moderate, and heavy intensity',
-    75: 'Snow fall: Slight, moderate, and heavy intensity',
-    77: 'Snow grains',
-    80: 'Rain showers: Slight, moderate, and violent',
-    81: 'Rain showers: Slight, moderate, and violent',
-    82: 'Rain showers: Slight, moderate, and violent',
-    85: 'Snow showers slight and heavy',
-    86: 'Snow showers slight and heavy',
-    95: 'Thunderstorm: Slight or moderate',
-    96: 'Thunderstorm with slight and heavy hail',
-    99: 'Thunderstorm with slight and heavy hail',
-  };
-  public weatherIcons: { [key: number]: string } = {
-    0: '../assets/sun.png',
-    1: '../assets/partly-clear.png',
-    2: '../assets/partly-clear.png',
-    3: '../assets/partly-clear.png',
-    45: '../assets/foggy.png',
-    48: '../assets/foggy.png',
-    51: '../assets/drizzle.png',
-    53: '../assets/drizzle.png',
-    55: '../assets/drizzle.png',
-    56: '../assets/freezing-drizzle.png',
-    57: '../assets/freezing-drizzle.png',
-    61: '../assets/rainy.png',
-    63: '../assets/rainy.png',
-    66: '../assets/heavy-rain.png',
-    67: '../assets/heavy-rain.png',
-    71: '../assets/snow.png',
-    73: '../assets/snow.png',
-    75: '../assets/snow.png',
-    77: '../assets/snow.png',
-    80: '../assets/heavy-rain.png',
-    81: '../assets/heavy-rain.png',
-    82: '../assets/heavy-rain.png',
-    85: '../assets/snow.png',
-    86: '../assets/snow.png',
-    95: '../assets/scattered-thunderstorms.png',
-    96: '../assets/thunderstorm.png',
-    99: '../assets/thunderstorm.png',
-  };
+  public weatherInterpretationCodes = weatherInterpretationCodes;
+  public weatherIcons = weatherIcons;
   public currentIconImgPath: string = '';
   public currentDate: string | null = null;
   public weekForecastArray: any[] = [];
@@ -95,14 +41,12 @@ export class AppComponent {
     this.showCitites = false;
     if (this.searchInput.length < 3) {
       this.citiesList = [];
-      // console.log('input <3');
     }
     if (this.searchInput.length >= 3) {
       this.weatherService.searchCity(this.searchInput).subscribe(
         (cityResult) => {
           this.citiesList = cityResult.results;
           this.showCitites = true;
-          //console.log(this.citiesList);
         },
         (error) => {
           console.log(error);
@@ -112,7 +56,6 @@ export class AppComponent {
   }
 
   public onCityClick(city: City): void {
-    // console.log(city);
     this.weekForecastArray = []; //reset the weekly forecast
     this.weatherService.getCityWeather(city).subscribe((cityWeather) => {
       this.currentCityWeather = cityWeather;
@@ -123,10 +66,11 @@ export class AppComponent {
       this.citiesList = [];
       this.currentCity = city;
       this.mapWeekForecast();
-
-      console.log(this.currentCity.id);
-      console.log(this.currentCity);
     });
+  }
+
+  public showSearch(): void {
+    this.showCitites = true;
   }
 
   public hideCititesList(): void {
@@ -135,10 +79,46 @@ export class AppComponent {
     }, 300);
   }
 
+  public formatIcon(code: number): string {
+    return this.weatherIcons[code];
+  }
+
+  public addToFavourites(city: City, cityWeather: Forecast) {
+    if (
+      !this.favouriteCities.find(
+        (favouriteCity) => favouriteCity.name === city.name
+      )
+    ) {
+      const newFavourite: City = {
+        id: city.id,
+        name: city.name,
+        country: city.country,
+        latitude: city.latitude,
+        longitude: city.longitude,
+        timezone: city.timezone,
+        temp: cityWeather.current_weather.temperature,
+        weathercode: cityWeather.current_weather.weathercode,
+        tempMax: cityWeather.daily.temperature_2m_max,
+        tempMin: cityWeather.daily.temperature_2m_min,
+        precip: cityWeather.daily.precipitation_probability_max,
+      };
+      this.favouriteCities.push(newFavourite);
+      this.saveToLocalStorage();
+    } else alert('City already added to favourites.');
+  }
+
+  public removeFavourite(favouriteCity: City): void {
+    const cityIndex = this.favouriteCities.findIndex(
+      (city) => city.name === favouriteCity.name
+    );
+
+    this.favouriteCities.splice(cityIndex, 1);
+    this.saveToLocalStorage();
+  }
+
   //
   private getCurrentLocationDetails(): void {
     navigator.geolocation.getCurrentPosition((response) => {
-      // console.log(response);
       this.geoLocationService
         .getCityByCoordinates(
           response.coords.latitude,
@@ -187,48 +167,12 @@ export class AppComponent {
         };
         this.weekForecastArray.push(weekDay);
       }
-      // console.log('array forecast', this.weekForecastArray);
+
       this.currentDate = this.datePipe.transform(
         this.currentCityWeather.current_weather.time,
         'fullDate'
       );
     }
-  }
-  public formatIcon(code: number): string {
-    return this.weatherIcons[code];
-  }
-
-  public addToFavourites(city: City, cityWeather: Forecast) {
-    if (
-      !this.favouriteCities.find(
-        (favouriteCity) => favouriteCity.name === city.name
-      )
-    ) {
-      const newFavourite: City = {
-        id: city.id,
-        name: city.name,
-        country: city.country,
-        latitude: city.latitude,
-        longitude: city.longitude,
-        timezone: city.timezone,
-        temp: cityWeather.current_weather.temperature,
-        weathercode: cityWeather.current_weather.weathercode,
-        tempMax: cityWeather.daily.temperature_2m_max,
-        tempMin: cityWeather.daily.temperature_2m_min,
-        precip: cityWeather.daily.precipitation_probability_max,
-      };
-      this.favouriteCities.push(newFavourite);
-      this.saveToLocalStorage();
-    } else alert('City already added to favourites.');
-  }
-
-  public removeFavourite(favouriteCity: City): void {
-    const cityIndex = this.favouriteCities.findIndex(
-      (city) => city.name === favouriteCity.name
-    );
-    // console.log(cityIndex);
-    this.favouriteCities.splice(cityIndex, 1);
-    this.saveToLocalStorage();
   }
 
   private saveToLocalStorage(): void {
